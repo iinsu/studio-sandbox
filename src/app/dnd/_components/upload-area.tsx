866dbcf05eb683ad13e29116d2428195af098651
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-01-10 18:48:11 +0900

    업로드 하려는 파일 개수 확인

--- src/app/dnd/_components/upload-area.tsx
+++ src/app/dnd/_components/upload-area.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 
-export const DragAndDropArea = ({ onUpload, children }: any) => {
+export const DragAndDropArea = ({ onUpload, children, count = 1 }: any) => {
   const drop = useRef<HTMLDivElement>(null);
 
   const handleDragOver = (event: DragEvent) => {
@@ -15,9 +15,17 @@ export const DragAndDropArea = ({ onUpload, children }: any) => {
     event.stopPropagation();
 
     const result = event.dataTransfer;
-    if (result?.files && result.files.length) {
-      onUpload(result.files);
+
+    // 값이 NULL 일 경우 처리
+    if (result === null) return;
+
+    // Props 로 넘어온 Count 보다 등록하려는 파일 개수가 많을 때 처리
+    if (count < result.files.length) {
+      console.log(`파일 업로드는 ${count}개만 가능합니다.`);
+      return;
     }
+
+    onUpload(result.files);
   };
 
   useEffect(() => {
