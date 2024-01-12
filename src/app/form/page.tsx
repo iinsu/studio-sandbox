dde80681c1d0bd02d1eeacc5c4c060803bb8ef28
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-01-12 13:57:27 +0900

    Update FieldArray Page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,9 +1,115 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+
+const sampleFormSchema = z.object({
+  username: z.string(),
+  urls: z.array(
+    z.object({
+      value: z.string().url({ message: "Please enter a valid URL." }),
+    })
+  ),
+});
+
+type SampleFormValues = z.infer<typeof sampleFormSchema>;
+
+const defaultValues: Partial<SampleFormValues> = {
+  username: "Sample",
+  urls: [
+    {
+      value: "https://sample1.com",
+    },
+    {
+      value: "https://smaple2.com",
+    },
+  ],
+};
+
 const HookForm = () => {
+  const form = useForm<SampleFormValues>({
+    resolver: zodResolver(sampleFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const { fields, append } = useFieldArray({
+    name: "urls",
+    control: form.control,
+  });
+
+  const onSubmit = (data: SampleFormValues) => {
+    console.log("submit", data);
+  };
+
   return (
     <>
-      <div>Hook Form</div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name. It can be your real name or
+                  a pseudonym. You can only change this once every 30 days.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div>
+            {fields.map((field, index) => (
+              <FormField
+                control={form.control}
+                key={field.id}
+                name={`urls.${index}.value`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className={cn(index !== 0 && "sr-only")}>
+                      URLs
+                    </FormLabel>
+                    <FormDescription className={cn(index !== 0 && "sr-only")}>
+                      dd links to your website, blog, or social media profiles.
+                    </FormDescription>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
+            <Button
+              type="button"
+              variant="outline"
+              size="sm"
+              className="mt-2"
+              onClick={() => append({ value: "" })}
+            >
+              Add URL
+            </Button>
+          </div>
+        </form>
+      </Form>
     </>
   );
 };
