3cdcdb355c0bd36c89084ea920d2d449e37afb95
insu <118152974+rmsoft-insu@users.noreply.github.com>
2024-04-24 17:41:31 +0900

    Update test.tsx

--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import _ from "lodash";
+import { useSearchParams } from "next/navigation";
 
 export const TestComponent = (props: URLSearchParams) => {
+  const searchParams = useSearchParams();
   console.log("proop", props);
   console.log("empty", _.isEmpty(props));
+  console.log("useSearchParams", searchParams.get("test"));
   return (
     <>
       <div>Main</div>
